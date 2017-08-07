0529fc3c148ceaec22bb339b3739e039f58e1a7c
Rafael Correia <rafaelcbm@gmail.com>
2017-08-07 15:32:48 -0300

    - Correção no filtro de lancamentos. - Refatoração.

--- src/app/lancamentos/lancamentos-list.component.ts
+++ src/app/lancamentos/lancamentos-list.component.ts
@@ -15,68 +15,84 @@ import { Lancamento } from "../models/models.module";
 })
 export class LancamentosListComponent implements OnInit {
 
-	lancamentos$: Observable<Lancamento[]>;
-
 	lancamentos: Lancamento[];
 
 	constructor(private lancamentosService: LancamentosService, private filtroLancamentoService: FiltroLancamentoService) { }
 
 	ngOnInit() {
 
-		this.lancamentos$ = this.lancamentosService.dataObservable$;
+		this.inicializarLancamentos();
 
-		this.lancamentosService.dataObservable$.subscribe(lancamentos => {
-			this.lancamentos = lancamentos;
-			this.lancamentos = this.lancamentos.map(
-				(lancamento: any) => {
-					lancamento.show = true;
-					return lancamento;
-				});
-			console.log('Lancamentos no subscribe = ', this.lancamentos)
+		this.inicializarFiltroCompetencia();
 
-		});
+		this.inicializarFiltroContas();
 
-		this.filtroLancamentoService.competenciaLancamento$
-			.debounceTime(500)
-			.distinctUntilChanged()
-			.subscribe(novaCompetencia => this.lancamentosService.getByCompetencia(novaCompetencia));
-
-		this.filtroLancamentoService.selectedContas$
-			.subscribe(contasSelecionadas => {
-				if (contasSelecionadas.length > 0) {
-					this.lancamentos.forEach((lancamento: any) => {
-						lancamento.show = false;
-
-						if (contasSelecionadas.some(contasSelecionada => contasSelecionada == lancamento.conta.nome)) {
-							lancamento.show = true;
-						}
-					});
-				}
-			});
-
-		this.filtroLancamentoService.selectedCategorias$
-			.subscribe(categoriasSelecionadas => {
-
-				if (categoriasSelecionadas.length == 0) {
-					this.lancamentos.forEach((lancamento:any) => lancamento.show = true);
-				}
-
-				if (categoriasSelecionadas.length > 0) {
-					this.lancamentos.forEach((lancamento: any) => {
-						lancamento.show = false;
-
-						if (categoriasSelecionadas.some(categoriaSelecionada => categoriaSelecionada == lancamento.categoria.nome)) {
-							lancamento.show = true;
-						}
-					});
-				}
-			});
+		this.inicializarFiltroCategorias();
 
 		let competenciaAtual = moment().format('YYYYMM');
 		this.lancamentosService.getByCompetencia(competenciaAtual);
 	}
 
+	inicializarLancamentos(){
+		this.lancamentosService.dataObservable$.subscribe(lancamentos => {
+			this.lancamentos = lancamentos;
+			this.lancamentos = this.lancamentos.map(
+				(lancamento: any) => {
+					lancamento.showConta = true;
+					lancamento.showLancamento = true;
+					return lancamento;
+				});
+		});
+	}
+
+
+	inicializarFiltroCompetencia(){
+		this.filtroLancamentoService.competenciaLancamento$
+			.debounceTime(500)
+			.distinctUntilChanged()
+			.subscribe(novaCompetencia => this.lancamentosService.getByCompetencia(novaCompetencia));
+	}
+
+	inicializarFiltroContas(){
+		this.filtroLancamentoService.selectedContas$
+			.subscribe(contasSelecionadas => {
+				if (contasSelecionadas.length > 0) {
+					this.lancamentos.forEach((lancamento: any) => {
+						lancamento.showConta = false;
+
+						if (contasSelecionadas.some(contasSelecionada => contasSelecionada == lancamento.conta.nome)) {
+							lancamento.showConta = true;
+						}
+					});
+				}
+			});
+	}
+
+	inicializarFiltroCategorias(){
+		this.filtroLancamentoService.selectedCategorias$
+			.subscribe(categoriasSelecionadas => {
+
+				if (categoriasSelecionadas.length == 0) {
+					this.lancamentos.forEach((lancamento:any) => lancamento.showLancamento = true);
+				}
+
+				if (categoriasSelecionadas.length > 0) {
+					this.lancamentos.forEach((lancamento: any) => {
+						lancamento.showLancamento = false;
+
+						if (categoriasSelecionadas.some(categoriaSelecionada => categoriaSelecionada == lancamento.categoria.nome)) {
+							lancamento.showLancamento = true;
+						}
+					});
+				}
+			});
+	}
+
 	onSelect(lancamento: Lancamento) {
 		this.filtroLancamentoService.selectLancamento(lancamento);
+	}
+
+	showLancamento(lancamento: any) {
+		return lancamento.showConta && lancamento.showLancamento;
 	}
 }
